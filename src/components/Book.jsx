6a9e4f646ae2cc5769881951d94323c06acea0e8
Sergio Peralta <seperalta88@gmail.com>
2023-06-07 18:10:36 -0300

    Create book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,2 +1,17 @@
-const Book = () => <div>Book</div>;
+import PropTypes from 'prop-types';
+
+function Book({ title, author }) {
+	return (
+		<div>
+			<h3 className='book-title'>{title}</h3>
+			<p className='book-author'>{author}</p>
+			<button type='button'>Delete</button>
+		</div>
+	);
+}
+Book.propTypes = {
+	title: PropTypes.string.isRequired,
+	author: PropTypes.string.isRequired,
+};
+
 export default Book;
