1f1025bb727252c7cfacff14e5cc44e73a8301fd
Sergio Peralta <seperalta88@gmail.com>
2023-06-12 14:26:28 -0300

    Create remove book feature

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -9,9 +9,9 @@ const BookList = () => {
       {books.map((book) => (
         <Book
           key={book.item_id}
+          id={book.item_id}
           title={book.title}
           author={book.author}
-          category={book.category}
         />
       ))}
     </div>
